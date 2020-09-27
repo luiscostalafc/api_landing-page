685628f3aa13c75b23e130dbcedd27b64ceaabcd
luiscostalafc <luiscostalafc@gmail.com>
2020-09-27 15:51:28 -0300

    update plugins config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: "América do Sul (São Paulo)",
+      region: "aws-region",
       params: {
         Bucket: "landing-page-test-assets",
       }
